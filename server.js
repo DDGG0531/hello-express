16ff60459b2501f340384ccbc84b2acae3c1b28a
JimDong <jimdong@damaiapp.com.tw>
2022-10-31 12:31:29 +0800

    fix port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 // Express Initialize
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
