85e6ebcad9b3aa70dde5204e7655ddf9bac48568
JimDong <jimdong@damaiapp.com.tw>
2022-10-31 12:16:55 +0800

    add config

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require("express");
 // Express Initialize
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!");
 });
 
 app.listen(port, () => {
