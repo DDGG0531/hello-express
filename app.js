dd81a5b43bd814784b4a5ba7bad75f5ca2b213ca
JimDong <jimdong@damaiapp.com.tw>
2022-10-31 11:28:53 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Require module
+const express = require("express");
+// Express Initialize
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
